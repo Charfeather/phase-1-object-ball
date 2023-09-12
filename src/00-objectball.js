ebcc17d8417a6cf60bdc55b801d1c04649856846
charlesfeatherstone <charlesfstone11@gmail.com>
2023-09-12 15:58:46 -0400

    workStart

---
+++ src/00-objectball.js
@@ -0,0 +1,21 @@
+function gameObject(){
+    const obj={
+        home:{
+            teamName:"",
+            colors:"",
+            players:{
+                "Allan Anderson":{},
+                "Reggie Evans":{},
+                "Brook Lopez":{},
+                "Mason Plumlee":{},
+                "Jason Terry":{},
+            },
+        },
+        away:{
+            teamName:"",
+            colors:"",
+            players:"",
+        }
+    }
+    return obj
+}
